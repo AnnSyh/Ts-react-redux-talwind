4811669e8604b6d88b98089b2f87de1ac9c028d8
AnnSyh <ann.syharevskaya@gmail.com>
2022-12-09 18:00:02 +0300

    start ts+react+redux

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='container'>
+        hello!
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redusers/index.ts
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export const redusers = combineReducers({
+  counter: () => 0,
+});
